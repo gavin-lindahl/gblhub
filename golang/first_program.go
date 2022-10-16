1951b62238f0a12706b3dbbf8007dabeb312f21e
gavin-lindahl <gavin.lindahl@gmail.com>
2022-10-16 12:46:00 -0500

    Updated first_program.go

--- golang/first_program.go
+++ golang/first_program.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func addition(a int, b int) int {
+	return a + b
+}
+
+func subtraction(a int, b int) int {
+	return a - b
+}
+
+func division(a int, b int) int {
+	return a / b
+}
+
+func sum(nums ...int) {
+	fmt.Println(nums, " ")
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	sum(1, 2)
 }
